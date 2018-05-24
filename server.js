e27c8acbbc7faff2c0266e01bf4275f29dea692d
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-05-24 18:19:33 +0530

    Projects page added

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
